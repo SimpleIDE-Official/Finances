8ef192d585ff485e5406514d0a95449345084e4c
Thomas Schmid <1441369+scto@users.noreply.github.com>
2026-01-21 18:16:28 +0100

    Refine comments in build.gradle.kts
    Updated comments to clarify purpose of dependencies.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,15 +9,14 @@ repositories {
 }
 
 dependencies {
-    // Stellt sicher, dass Kotlin in buildSrc verfügbar ist
+    // Stellt das Kotlin Gradle Plugin bereit (inkl. Parcelize Logik)
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
 
-    // WICHTIG: Dies stellt die Klasse 'com.android.build.gradle.BaseExtension' 
-    // für deine Skripte in buildSrc zur Verfügung.
-    // Wenn du eine andere AGP Version nutzt, passe sie hier an.
+    // Stellt die Android Gradle Klassen bereit (BaseExtension etc.)
+    // Achtung: Version muss kompatibel mit deiner Dependencies.kt sein.
     implementation("com.android.tools.build:gradle:8.2.2")
     
-    // Falls du XML Parsing oder andere Utils in buildSrc nutzt:
+    // Hilfreich für Plugin-Entwicklung
     implementation(gradleApi())
     implementation(localGroovy())
 }
