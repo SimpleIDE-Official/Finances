a278893f256ed561219128349e404f32ad042f5f
Thomas Schmid <1441369+scto@users.noreply.github.com>
2026-01-21 16:01:15 +0100

    Update repository configuration in build.gradle.kts
    Replace jcenter() with a specific Maven repository URL.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,9 +4,12 @@ plugins {
 
 repositories {
     google()
-    jcenter()
     mavenCentral()
+    maven {
+      url = uri("https://jcenter.bintray.com/")
+    } // Wichtig für serg.chuprin
 }
+
 dependencies {
     implementation("com.android.tools.build:gradle:9.0.0")
 }
