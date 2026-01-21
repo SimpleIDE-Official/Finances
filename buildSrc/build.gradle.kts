ff1811a9fbc7f16544ed04fb8c1d463e0fabd32f
Thomas Schmid <1441369+scto@users.noreply.github.com>
2026-01-21 17:58:57 +0100

    Update Kotlin Gradle plugin and add new dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,6 +11,12 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:9.0.0"
-    implementation(kotlin("gradle-plugin", "1.6.10"))
+    implementation("com.android.tools.build:gradle:9.0.0")
+    implementation(kotlin("gradle-plugin", "1.9.20"))
+    implementation("com.google.gms:google-services:4.3.4")
+    implementation("de.mannodermaus.gradle.plugins:android-junit5:1.10.0.0")
+    implementation("com.vanniktech:gradle-dependency-graph-generator-plugin:0.5.0")
+    implementation("gradle.plugin.ru.cleverpumpkin.proguard-dictionaries-generator:plugin:1.0.8")
+    implementation("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.2"
+    }
 }
