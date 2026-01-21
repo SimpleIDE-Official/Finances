89a918de45383931287a65f3d65445f3d9014da3
Thomas Schmid <1441369+scto@users.noreply.github.com>
2026-01-21 16:20:11 +0100

    Update JUnit5 plugin version to 1.10.0.0

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -15,7 +15,7 @@ object BuildScript {
     object Plugins {
         const val GMS = "com.google.gms:google-services:4.3.4"
         const val ANDROID = "com.android.tools.build:gradle:9.0.0"
-        const val JUNIT5 = "de.mannodermaus.gradle.plugins:android-junit5:1.7.0.0"
+        const val JUNIT5 = "de.mannodermaus.gradle.plugins:android-junit5:1.10.0.0"
         const val KOTLIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:$KOTLIN_VER"
         const val NAVIGATION =
             "androidx.navigation:navigation-safe-args-gradle-plugin:$NAVIGATION_VER"
